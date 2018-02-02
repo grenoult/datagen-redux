d1b02759611e413566a394f2556ba8e3d22f8234
Guillaume Renoult <guillaume.renoult@elmolearning.com.au>
2018-02-02 15:58:24 +1100

    Updated reducer code

--- src/reducers/form.js
+++ src/reducers/form.js
@@ -1,37 +1,25 @@
-import {FORM_LOADING} from "../actions";
+import {FORM_LOADED, FORM_LOADING} from "../actions";
 
 const initialState =  {
-    // form: {
-        loaded: false,
-        loading: false,
-        data: []
-    // },
-    // result: {
-    //     loaded: false,
-    //     loading: false,
-    //     data: []
-    // }
+    loaded: false,
+    loading: false,
+    data: []
 };
 
 function form(state = initialState, action) {
-    // Somehow calculate it...
-    console.log('FormReducer!');
-    console.log(state);
-    // console.log(action);
+    // console.log('FormReducer!');
+    // console.log(state);
 
 
     switch (action.type) {
         case FORM_LOADING:
-            var newState = Object.assign({}, state);
-            newState.loading = true;
-            return newState;
-        // return Object.assign({}, state, {
-        //     loading: true
-        // });
-        // state.form.loading = true;
-        // console.log('State updated');
-        // console.log(state);
-        // return state;
+            return Object.assign({}, state,{
+                loading: true
+            });
+        case FORM_LOADED:
+            return Object.assign({}, state,{
+                loaded: true
+            });
 
         default:
             return state;
