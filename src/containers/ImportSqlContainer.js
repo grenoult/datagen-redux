948e81b380de55cb670039c81da50b52241ec563
Guillaume Renoult <guillaume.renoult@gmail.com>
2022-03-01 07:54:02 +1100

    Add reset action (redux) to clear result after changing page
    This is to avoid weird results when switching between pages.

--- src/containers/ImportSqlContainer.js
+++ src/containers/ImportSqlContainer.js
@@ -2,26 +2,28 @@ import React from 'react'
 import { connect } from 'react-redux'
 import ImportSqlTextarea from '../components/ImportSqlTextarea';
 import ResultContainer from './ResultContainer';
+import { resetResult } from '../actions/result';
 
 class ImportSqlContainerComponent extends React.Component {
-    render() {
-        return (<div>
-            <h1>Import SQL</h1>
-            <p>Upload a SQL File or paste a SQL script: we will generate data based on table structure.</p>
-            <ImportSqlTextarea></ImportSqlTextarea>
-            <ResultContainer/>
-        </div>);
-    }
+  componentDidMount() {
+    this.props.resetResult();
+  }
+
+  render() {
+    return (<div>
+      <h1>Import SQL</h1>
+      <p>Upload a SQL File or paste a SQL script: we will generate data based on table structure.</p>
+      <ImportSqlTextarea></ImportSqlTextarea>
+      <ResultContainer/>
+    </div>);
+  }
 }
 
 const mapStateToProps = state => ({
-    // loading: state.result.loading,
-    // loaded: state.result.loaded,
-    // result: state.result.data,
 });
 
 const mapDispatchToProps = {
-    // getResultFromScript,
+  resetResult
 };
 
 const ImportSqlContainer = connect(
